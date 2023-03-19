bfa6f642d1e73faca7a65c9d963ccc434c201d50
Bao Minh <haphanabominh9674@gmail.com>
2023-03-20 01:25:30 +0700

    allowe host

--- nettruyen/settings.py
+++ nettruyen/settings.py
@@ -130,3 +130,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'vietdev-nettruyen.onrender.com']
